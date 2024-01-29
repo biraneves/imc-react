9aee141e61c65fc852ff48cf54321671ff16415c
Ubirajara Neves <ubirajara.neves@usp.br>
2024-01-29 16:58:10 +0100

    Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+
+function App() {
+    return (
+        <div className="container">
+            <div className="body-app">
+                <Header />
+            </div>
+        </div>
+    );
+}
+
+export default App;
